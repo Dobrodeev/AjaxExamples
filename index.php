d7575d52ff6273a94dac4a38ce0c41bf5a8435c7
dobrodeev <dobrod@meta.ua>
2018-12-13 23:48:16 +0200

    в файле page1.html произвели разложение массива, который находится в файле index.php в json формат

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 29.11.2018
+ * Time: 16:54
+ */
+/**
+header('Content-Type: text/html; charset=UTF-8');
+echo '<html>';
+echo '<body>';
+echo '<div><b>Привет я сервер!</b></div>';
+echo '</body>';
+echo '</html>';
+ */
+//echo date('H:i:s');
+//echo 'Privet'.' '.$_POST['username'];
+$arr['object'] = 'value';
+$arr['empty'] = 'false';
+$arr['sec'] = 1968;
+$arr['valid'] = true;
+$arr['size'] = 1;
+echo json_encode($arr);
+
