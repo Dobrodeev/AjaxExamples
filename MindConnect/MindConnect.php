ebfdaf358dd453256055c2019d0a5ba033d3cbb9
Dobrodeev <dobrod@meta.ua>
2019-11-23 12:35:15 +0200

    написали 3 статических метода.

--- MindConnect/MindConnect.php
+++ MindConnect/MindConnect.php
@@ -6,18 +6,33 @@ class MindConnect
     /*returns the length of the
     last word of a given sentence, or 0 for empty string*/
     public static function last_word($sentence){
-
+        if ($sentence == '') return 0;
+        $words = explode(" ",$sentence);
+        $words = $words[count($words) - 1];
+        return strlen($words);
     }
     /*returns a date
     formatted in sql date format*/
     public static function sql_date_format($dateStr){
-
+        return strftime("%Y-%m-%d", strtotime($dateStr));
     }
 
     /*returns a part of a string
     that is marked with square parenthesis, if exists. For example: for "The quick
     [brown fox]." It will return "brown fox". For "Hello World" it will return ""*/
     public static function extract_string($str){
-
+        $mas_parenthesis = array();
+        $work_str = $str;
+        while (1){
+            $pos1 = strpos($work_str, '[');
+            $pos2 = strpos($work_str, ']');
+            if ($pos1 === false || $pos2 === false) break;
+            $newInsert = substr($work_str, $pos1, $pos2 - $pos1 + 1);
+            $mas_parenthesis[] = $newInsert;
+//            var_dump($mas_parenthesis);
+            $work_str = str_replace($newInsert, '', $work_str);
+//            var_dump($work_str);
+        }
+        return $mas_parenthesis;
     }
 }
--- MindConnect/index.php
+++ MindConnect/index.php
@@ -1,5 +1,13 @@
 <?php
-spl_autoload_register(function ($class){
+/*spl_autoload_register(function ($class){
     include $class . '.class.php';
 });
-$stringMindConnect = new MindConnect();
+$stringMindConnect = new MindConnect();*/
+include 'MindConnect.php';
+echo MindConnect::last_word('returns the length of the
+    last word of a given sentence, or 0 for empty string');
+echo '<br>';
+echo MindConnect::sql_date_format(date('Y-m-d'));
+echo '<br>';
+print_r(MindConnect::extract_string('The quick
+    [brown fox]. Moscow [Kiev].'));
