3d2a923f085e14fba86dacc8f3aa2ff07151ee5d
Dobrodeev <dobrod@meta.ua>
2020-04-13 12:28:39 +0300

    добавили DocBlocks

--- MindConnect/MindConnect.php
+++ MindConnect/MindConnect.php
@@ -1,10 +1,19 @@
 <?php
 
 
+/**
+ * Class MindConnect
+ * @package workwithtext
+ * @author Dobrodeev V.
+ * @copyright 2019
+ */
 class MindConnect
 {
-    /*returns the length of the
-    last word of a given sentence, or 0 for empty string*/
+    /**
+     * returns the length of the last word of a given sentence, or 0 for empty string
+     * @param $sentence
+     * @return int
+     */
     public static function last_word($sentence)
     {
         if ($sentence == '') {
@@ -15,16 +24,22 @@ class MindConnect
         return strlen($words);
     }
 
-    /*returns a date
-    formatted in sql date format*/
+    /**
+     * returns a date formatted in sql date format
+     * @param $dateStr
+     * @return string
+     */
     public static function sql_date_format($dateStr)
     {
         return strftime("%Y-%m-%d", strtotime($dateStr));
     }
 
-    /*returns a part of a string
-    that is marked with square parenthesis, if exists. For example: for "The quick
-    [brown fox]." It will return "brown fox". For "Hello World" it will return ""*/
+    /**
+     * returns a part of a string that is marked with square parenthesis, if exists. For example: for "The quick
+     * [brown fox]." It will return "brown fox". For "Hello World" it will return ""
+     * @param $str
+     * @return array
+     */
     public static function extract_string($str)
     {
         $mas_parenthesis = array();
@@ -37,9 +52,7 @@ class MindConnect
             }
             $newInsert = substr($work_str, $pos1, $pos2 - $pos1 + 1);
             $mas_parenthesis[] = $newInsert;
-//            var_dump($mas_parenthesis);
             $work_str = str_replace($newInsert, '', $work_str);
-//            var_dump($work_str);
         }
         return $mas_parenthesis;
     }
