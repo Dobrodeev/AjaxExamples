140d55a310f9edb04aa487963acec4cb5689faf7
Dobrodeev <dobrod@meta.ua>
2021-01-08 19:02:43 +0200

    api key вынесли в константу

--- SpectralSoftware/test.php
+++ SpectralSoftware/test.php
@@ -9,10 +9,10 @@
  * @link https://openweathermap.org/forecast5
  */
 //https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
-
+define('API_KEY', '731fdb9f46272f54a8b68c894765410b');
 // адрес запроса&mode=xml
 //api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7
-$url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Kiev&units=metric&cnt=5&lang=ua&apikey=731fdb9f46272f54a8b68c894765410b";
+$url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Kiev&units=metric&cnt=5&lang=ua&apikey=API_KEY";
 //$url = "http://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&cnt=5&units=metric&lang=ua&apikey=731fdb9f46272f54a8b68c894765410b";
 //$url = "https://samples.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=731fdb9f46272f54a8b68c894765410b";
 //$url = 'http://sms2.cdyne.com/sms.svc/SimpleSMSsendWithPostback? PhoneNumber=18887477474&Message=test&LicenseKey=LICENSEKEY';
@@ -20,6 +20,7 @@ $cURL = curl_init();
 curl_setopt($cURL, CURLOPT_URL, $url);
 curl_setopt($cURL, CURLOPT_HTTPGET, true);
 curl_setopt($cURL, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($cURL, CURLOPT_RETURNTRANSFER, false);
 curl_setopt($cURL, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
 $result = curl_exec($cURL);
 curl_close($cURL);
